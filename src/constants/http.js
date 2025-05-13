8d1cddd14031c722ec519ad702ee48f8dc2e7371
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-05-13 08:49:59 +0700

    fix indent in constants

--- src/constants/http.js
+++ src/constants/http.js
@@ -1,12 +1,12 @@
 export const HTTP_STATUS = {
-  OK: 200,
-  CREATED: 201,
-  BAD_REQUEST: 400,
-  NOT_FOUND: 404,
-  SERVER_ERROR: 500,
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    SERVER_ERROR: 500,
 };
 
 export const RESPONSE_STATUS = {
-  SUCCESS: 'success',
-  FAIL: 'fail',
+    SUCCESS: 'success',
+    FAIL: 'fail',
 };
