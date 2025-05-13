2726e388e69dc846aef5f1d036a8403ecabcfb00
Umar Hadi Mukti <umarhadimukti@gmail.com>
2025-05-13 08:50:43 +0700

    fix indent server js

--- src/server.js
+++ src/server.js
@@ -2,25 +2,25 @@ import Hapi from '@hapi/hapi';
 import routes from './routes/book.js';
 
 const init = async () => {
-  const server = Hapi.server({
-    port: 9000,
-    host: 'localhost',
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
-    },
-  });
+    const server = Hapi.server({
+        port: 9000,
+        host: 'localhost',
+        routes: {
+        cors: {
+            origin: ['*'],
+        },
+        },
+    });
 
-  server.route(routes);
+    server.route(routes);
 
-  await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
-  console.log(err);
-  process.exit(1);
+    console.log(err);
+    process.exit(1);
 });
 
 init();
